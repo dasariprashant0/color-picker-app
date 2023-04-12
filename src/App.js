6d41cddc21349b1f626b6b999574c65147bd55d0
Prashant Dasari <dasariprashant0@gmail.com>
2023-04-13 00:00:44 +0530

    short challenge completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Input from "./Input";
+import Square from "./Square";
+
+function App() {
+  const [colorValue, setColorValue] = useState("");
+  const [hexValue, setHexValue] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  return (
+    <div className="App">
+      <Square
+        colorValue={colorValue}
+        hexValue={hexValue}
+        isDarkText={isDarkText}
+      />
+      <Input
+        colorValue={colorValue}
+        setColorValue={setColorValue}
+        setHexValue={setHexValue}
+        setIsDarkText={setIsDarkText}
+        isDarkText={isDarkText}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import colorNames from "colornames";
+
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  return (
+    <form action="" onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="">Add Color Name: </label>
+      <input
+        type="text"
+        autoFocus
+        placeholder="Add Color Name"
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
+      />
+      <button type="button" onClick={() => setIsDarkText(!isDarkText)}>Toggle Text Color</button>
+    </form>
+  );
+};
+
+export default Input;
